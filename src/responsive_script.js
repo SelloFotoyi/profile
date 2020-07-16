53c71c87d0657d1abc020b3023ec9db20d346fdb
Sello Fotoyi <selloclinton@gmail.com>
2020-07-16 12:38:43 +0200

    worked on sidenav links (now redirects) and footer images (redirect to icons folder)

--- src/responsive_script.js
+++ src/responsive_script.js
@@ -1,35 +1,35 @@
 $(function() {
     $("#twitterIcon").mouseenter(function () {
 
-        $("#twitterIcon").attr("src", "src/twitter_blue.png");
+        $("#twitterIcon").attr("src", "src/icons/twitter_blue.png");
         $(`#twitterIcon`).css({"width": "20px", "height": "20px"});
 
     });
     $("#twitterIcon").mouseleave(function () {
-        $("#twitterIcon").attr("src", "src/twitter_black.png");
+        $("#twitterIcon").attr("src", "src/icons/twitter_black.png");
         $("#twitterIcon").attr("title", "Twitter");
         $(`#twitterIcon`).css({"width": "20px", "height": "20px"});
     });
 
     $("#linkedInIcon").mouseenter(function () {
 
-        $("#linkedInIcon").attr("src", "src/linkedIn_blue.png");
+        $("#linkedInIcon").attr("src", "src/icons/linkedIn_blue.svg");
         $(`#linkedInIcon`).css({"width": "20px", "height": "20px"});
 
     });
     $("#linkedInIcon").mouseleave(function () {
-        $("#linkedInIcon").attr("src", "src/linkedIn_black.png");
+        $("#linkedInIcon").attr("src", "src/icons/linkedIn_black.png");
         $("#linkedInIcon").css({"width": "20px", "height": "20px"});
     });
 
     $("#githubIcon").mouseenter(function () {
 
-        $("#githubIcon").attr("src", "src/github_blue.svg");
+        $("#githubIcon").attr("src", "src/icons/github_blue.svg");
         $(`#githubIcon`).css({"width": "20px", "height": "20px"});
 
     });
     $("#githubIcon").mouseleave(function () {
-        $("#githubIcon").attr("src", "src/github_black.png");
+        $("#githubIcon").attr("src", "src/icons/github_black.png");
         $("#githubIcon").css({"width": "20px", "height": "20px"});
     });
 
@@ -63,7 +63,28 @@ $(function() {
 
         $("#openSideNav").show();
     });
+    $("#toProjects").on("click", function () {
+        $("#sideNav").animate({
+            width: "0"
+        },500);
 
+        $('document','body').animate({
+            scrollTop: $("#projects").offset().top
+        },1000);
+
+        $("#openSideNav").show();
+    });
+    $("#toSocials").on("click", function () {
+        $("#sideNav").animate({
+            width: "0"
+        },500);
+
+        $('document','body').animate({
+            scrollTop: $("#socials").offset().top
+        },500);
+
+        $("#openSideNav").show();
+    });
     let musicAppImage = $("<div></div>").html($("#musicAppImage").html());
     $("#musicAppImage").mouseenter(function(){
         $(this).html($("#musicAppDetail").html());
