5f818a8308bc9a9fe4ec0ca8cfa68a8fac53ab13
Sello Fotoyi <selloclinton@gmail.com>
2021-01-24 12:22:51 +0200

    home and nav issue fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menuBtn__burger = document.querySelector('.menu-btn__burger');
 let nav = document.querySelector('.nav');
+let home_logo = document.querySelector('.home-logo');
+let social_icons = document.querySelector('.social-icons');
 
 let showMenu = false;
 
@@ -10,11 +12,14 @@ function toggleOpen(){
     if(!showMenu){
         menuBtn__burger.classList.add('open');
         nav.classList.add('open');
+        home_logo.classList.add('open');
+        social_icons.classList.add('open');
         showMenu = true;
-        console.log('open');
     }else if(showMenu){
         menuBtn__burger.classList.remove('open');
         nav.classList.remove('open');
+        home_logo.classList.remove('open');
+        social_icons.classList.remove('open');
         showMenu = false;
     }
     
